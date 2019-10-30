2b88bbaa9bee91c69dcc019e6d18e6102b070501
Zac Smith <zrs3141592@gmail.com>
2019-10-29 21:44:25 -0600

    create end points

--- index.js
+++ index.js
@@ -23,7 +23,74 @@ server.get("/hubs", (req, res) => {
     })
     .catch(error => {
       res.status(500).json({
+        success: false,
         message: `GET did not work: "${error}"`
+      });
+    });
+});
+
+server.get("/hubs/:id", (req, res) => {});
+
+server.post("/hubs", (req, res) => {
+  const hubInfo = req.body;
+  db.add(hubInfo)
+    .then(hub => {
+      res.status(201).json({
+        success: true,
+        hub
+      });
+    })
+    .catch(error => {
+      res.status(500).json({
+        success: false,
+        message: `GET did not work: "${error}"`
+      });
+    });
+});
+
+server.delete("/hubs/:id", (req, res) => {
+  // const { id } = req.params.id;
+  const id = req.params.id;
+
+  db.remove(id)
+    .then(deletedHub => {
+      if (deletedHub) {
+        res.status(204);
+      } else {
+        res.status(404).json({
+          success: false,
+          message: `DELETE could not find id: ${id}`
+        });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        // message: `GET did not work: "${error}"`
+        sucess: false,
+        error
+      });
+    });
+});
+
+server.put("/hubs/:id", (req, res) => {
+  const id = req.params.id;
+  const hubInfo = req.body;
+
+  db.update(id, hubInfo)
+    .then(hub => {
+      if (hub) {
+        res.status(200).json({ success: true, hub });
+      } else {
+        res
+          .status(404)
+          .json({ success: false, message: `id ${id} can not be updated` });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        // message: `GET did not work: "${error}"`
+        sucess: false,
+        error
       });
     });
 });
